fe1eea07a905fd685e5c6a402656dd8c448249ad
Preet Patel <preet2222patel@gmail.com>
2022-03-10 18:59:17 +0530

    Third Commit

--- src/components/feelFood/index.js
+++ src/components/feelFood/index.js
@@ -1,9 +1,38 @@
 import React from 'react'
+import axios from 'axios'
+import { useEffect,useState } from 'react'
 
-function feelFood() {
+
+function FeelFood() {
+  const [recipes,setRecipes] = useState([])
+  useEffect(async ()=>{
+    try{
+      let recipe_param 
+      const res = await axios({
+        method:'post',
+        headers:{
+          "accepts":"application/json"
+      },
+      params:{
+        'emotion':'happy'
+      },
+      url:'/comfortFood'
+      })
+      recipe_param = res.data.result[0]
+      console.log(recipe_param)
+    }catch(err){
+      console.log(err)
+    }
+  },[])
   return (
-    <div>feelFood</div>
+    <>
+    {/* {(recipes !== {} || recipes !== []) && console.log(recipes)}
+    
+    <div>{(recipes !== {} || recipes !== []) && recipes.result.map((recipe)=>{
+      return <h1>{recipe}</h1>
+    })}</div> */}
+    </>
   )
 }
 
-export default feelFood
+export default FeelFood
