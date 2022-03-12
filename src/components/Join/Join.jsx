d57f85b67775dc5ebd251a5fe48d292a927b2566
Umang <umangpachaury@gmail.com>
2022-03-12 13:38:32 +0530

    Changes done in Join Page

--- src/components/Join/Join.jsx
+++ src/components/Join/Join.jsx
@@ -11,7 +11,10 @@ function Join() {
               <input placeholder='Name' className='joinInput' type='text' onChange={(e)=>{setName(e.target.value)} } />
               <input placeholder='Room' className='joinInput mt-20' type='text' onChange={(e)=>{setRoom(e.target.value)}} />
               <Link onClick={(e)=>(!name || !room)?e.preventDefault():null} to={`/foodConnect/chat?name=${name}&room=${room}`}>
-                  <button className='button mt-20' type='submit'>Join</button>
+                  <button className='button mt-20'>Join</button>
+              </Link>
+              <Link to={'/'}>
+                <button className='button mt-20' type='submit'>Exit to the app</button>
               </Link>
           </div>
       </div>
